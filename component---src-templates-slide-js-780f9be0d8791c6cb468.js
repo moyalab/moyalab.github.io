c83255429f45bb834d26535d0754513f5b57d99b
moyalab <iammoyalab@gmail.com>
2020-06-03 15:02:02 +0900

    Updates

---
+++ component---src-templates-slide-js-780f9be0d8791c6cb468.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"1WVW":function(t,e,n){"use strict";n.r(e),n.d(e,"query",(function(){return i}));var r=n("q1tI"),a=n.n(r);e.default=function(t){var e=t.data,n=t.transition;return a.a.createElement("div",{style:{width:"100%"}},a.a.createElement("div",{style:n&&n.style,dangerouslySetInnerHTML:{__html:e.slide.html}}))};var i="2716726613"}}]);
+//# sourceMappingURL=component---src-templates-slide-js-780f9be0d8791c6cb468.js.map
